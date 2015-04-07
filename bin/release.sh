7acdeb213829c1de16827d7872a1f95b80c2e25b
Paul C Pederson <ppederson@esri.com>
2015-04-07 11:10:00 -0700

    fix release script

--- bin/release.sh
+++ bin/release.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 git branch -D temp-release-branch
 git checkout -b temp-release-branch
-grunt prepublish
 cp node_modules/patterns-color/scss/variables.scss dist/sass/calcite-web/base/
 git add -f dist/
 git commit -m "Release"
 git push upstream temp-release-branch
-npm run gh-release
+gh-release
 git checkout master
 git branch -D temp-release-branch
 git push upstream :temp-release-branch
