f926260947ea29217ae77a99cf22e8bea29343ae
Paul C Pederson <paul.c.pederson@gmail.com>
2015-08-13 10:41:50 -0700

    Updates

--- assets/js/libs/calcite-web.js
+++ assets/js/libs/calcite-web.js
@@ -1,4 +1,4 @@
-/* calcite-web - v0.11.1 - 2015-08-11
+/* calcite-web - v0.11.2 - 2015-08-13
 *  https://github.com/esri/calcite-web
 *  Copyright (c) 2015 Environmental Systems Research Institute, Inc.
 *  Apache 2.0 License */
@@ -9,7 +9,7 @@
   // └────────────┘
   // define all public api methods (excluding patterns)
   var calcite = {
-    version: 'v0.11.1',
+    version: 'v0.11.2',
     click: click,
     addEvent: addEvent,
     removeEvent: removeEvent,
