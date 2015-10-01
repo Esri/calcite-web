ad7adad52d24cf175615b746e46ff14cfa250108
Nikolas Wise <nikolaswise@gmail.com>
2015-10-01 12:22:31 -0700

    generate webfonts and sass from a folder of svgs

--- bin/webfontifier.js
+++ bin/webfontifier.js
@@ -4,15 +4,35 @@
 // └────────────────┘
 // Takes a big pile of .svg and makes a usable set of webfonts
 
+var path = require('path')
+var fs = require('fs')
 var webfontsGenerator = require('webfonts-generator')
 
-webfontsGenerator({
-  files: [
-    '../lib/img/icon/ui/announcement.svg',
-    '../lib/img/icon/ui/attachment.svg',
-  ],
-  dest: '../lib/fonts/
-}, function(error) {
-  if (error) console.log('Fail!', error)
-  else console.log('Done!')
-})
+var target  = path.join(process.cwd(), 'lib/img/icons/ui/')
+var dest    = path.join(process.cwd(), 'lib/fonts/')
+var cssDest = path.join(process.cwd(), 'lib/sass/calcite-web/icons/_ui.scss')
+var cssTemplate = path.join(process.cwd(), 'bin/webfont-template.hbs')
+
+fs.readdir(target, function (err, files) {
+    if (err) {
+      throw err
+    }
+    files = files.map(function (file) {
+      return path.join(target, file);
+    })
+    generate(files)
+});
+
+var generate = function(files) {
+  webfontsGenerator({
+    files: files,
+    dest: dest,
+    fontName: 'calcite-ui',
+    css: true,
+    cssDest: cssDest,
+    cssTemplate: cssTemplate
+  }, function(error) {
+    if (error) console.log('Fail!', error)
+    else console.log('Done!')
+  })
+}
