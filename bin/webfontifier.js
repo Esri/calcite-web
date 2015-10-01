2ebc74150f0afe0ad57ba28e98e5d1f7a6a22aa2
Nikolas Wise <nikolaswise@gmail.com>
2015-10-01 09:35:00 -0700

    sample code webfont generator

--- bin/webfontifier.js
+++ bin/webfontifier.js
@@ -3,3 +3,16 @@
 // │ Icon Fontifier │
 // └────────────────┘
 // Takes a big pile of .svg and makes a usable set of webfonts
+
+var webfontsGenerator = require('webfonts-generator')
+
+webfontsGenerator({
+  files: [
+    '../lib/img/icon/ui/announcement.svg',
+    '../lib/img/icon/ui/attachment.svg',
+  ],
+  dest: '../lib/fonts/
+}, function(error) {
+  if (error) console.log('Fail!', error)
+  else console.log('Done!')
+})
