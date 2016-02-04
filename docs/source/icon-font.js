253426d7fb7e165d6cc70142adba81de4cc7564d
Paul C Pederson <paul.c.pederson@gmail.com>
2016-02-03 17:22:24 -0800

    clean up docs source folder, remove leftover files

---
+++ docs/source/icon-font.js
@@ -0,0 +1,11 @@
+var path = require('path')
+var fs = require('fs')
+
+module.exports = function (callback) {
+  return fs.readdir('lib/img/icons/ui', function (er, files) {
+    files = files.map(function (filename) {
+      return path.basename(filename, '.svg')
+    })
+    callback(er, files);
+  })
+}
