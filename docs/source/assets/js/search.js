d77c881c652d7b0df4f92b6887e614d89a022b31
Nikolas Wise <nikolaswise@gmail.com>
2016-02-23 15:38:05 -0800

    filter dropdown working

--- docs/source/assets/js/search.js
+++ docs/source/assets/js/search.js
@@ -1,4 +1,7 @@
-console.log('cool search page')
+console.log('pretty cool search page guy')
+
+
+calcite.bus.on('filterDropdown:active', logActive)
 function logActive (options) {
   console.log(`Filter dropdown '${options.id}' has ${options.active.length} active items`)
   console.log(options.active)
