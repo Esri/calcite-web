ba458fa1c18e857558e11bbccfc4688a541712d5
Nikolas Wise <nikolaswise@gmail.com>
2016-02-26 13:25:24 -0800

    lint

--- docs/source/assets/js/search.js
+++ docs/source/assets/js/search.js
@@ -1,5 +1,1 @@
-calcite.bus.on('filterDropdown:active', logActive)
-function logActive (options) {
-  console.log(`Filter dropdown '${options.id}' has ${options.active.length} active items`)
-  console.log(options.active)
-}
+console.log('search pattern js');
