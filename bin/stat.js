7c728b109eed330f9986586ccf8db4ab401c3c35
Paul C Pederson <paul.c.pederson@gmail.com>
2016-05-25 10:10:06 -0700

    add semicolons

--- bin/stat.js
+++ bin/stat.js
@@ -1,6 +1,6 @@
-var stats = require('../dist/css/stats.json')
+var stats = require('../dist/css/stats.json');
 
-console.log('size:      ' + stats.humanizedSize)
-console.log('gzipped:   ' + stats.humanizedGzipSize)
-console.log('rules:     ' + stats.rules.total)
-console.log('selectors: ' + stats.selectors.total)
+console.log('size:      ' + stats.humanizedSize);
+console.log('gzipped:   ' + stats.humanizedGzipSize);
+console.log('rules:     ' + stats.rules.total);
+console.log('selectors: ' + stats.selectors.total);
