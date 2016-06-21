b2954b5c2225850ac8b83c2588562f83bd771aaf
Nikolas Wise <nikolaswise@gmail.com>
2016-06-21 15:19:42 -0700

    clean dat up

---
+++ docs/source/assets/js/clipboard.js
@@ -0,0 +1,7 @@
+var clipboard = new Clipboard('.js-copy-to-clipboard');
+
+clipboard.on('success', function(e) {
+  console.info('Action:', e.action);
+  console.info('Text:', e.text);
+  console.info('Trigger:', e.trigger);
+});
