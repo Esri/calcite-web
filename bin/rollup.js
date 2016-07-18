3e34d5bc309a3efca5fdeaea891cc2a13ee56786
Nikolas Wise <nikolaswise@gmail.com>
2016-07-18 10:19:38 -0700

    rollup rolling up

--- bin/rollup.js
+++ bin/rollup.js
@@ -1,8 +1,13 @@
 var rollup = require('rollup');
 var glob = require('glob');
 var path = require('path');
+var babel = require('rollup-plugin-babel');
 
 glob('./lib/js/**.js', {}, compile)
+
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 function compile (err, files) {
   if (err) {
@@ -10,12 +15,25 @@ function compile (err, files) {
     return
   }
   files.forEach(function (file) {
+
     var filename = path.basename(file);
+    var arr = filename.split('-')
+    var moduleName = arr[0]
+    var extension = arr[arr.length - 1]
+
+    if (extension != 'web.js') {
+      extension = extension.split('.')[0]
+      moduleName = moduleName + capitalize(extension)
+    }
     rollup.rollup({
-      entry: './dist/js/' + filename,
+      entry: './lib/js/' + filename,
+      plugins: [
+        babel({exclude: 'node_modules/**'})
+      ]
     }).then( function ( bundle ) {
       bundle.write({
         format: 'umd',
+        moduleName: moduleName,
         dest: './dist/js/' + filename
       });
     });
