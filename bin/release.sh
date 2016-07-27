ad4dff8de2f855589036c3231277285c39d39bbd
Nikolas Wise <nikolaswise@gmail.com>
2016-07-27 11:12:19 -0700

    :package: v1.0.0-beta-29

--- bin/release.sh
+++ bin/release.sh
@@ -2,6 +2,8 @@
 git branch -D release
 git checkout -b release
 git pull --rebase upstream release
+npm run rollup
+npm run sass:dist
 git add -f dist/
 git commit -m "Release"
 git push upstream release
