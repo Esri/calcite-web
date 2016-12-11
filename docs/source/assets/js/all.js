395fd5c7fa9ee8dd5f506f7c0833a550d406450e
Paul C Pederson <paul.c.pederson@gmail.com>
2016-12-10 16:47:21 -0800

    simplify docs js

--- docs/source/assets/js/all.js
+++ docs/source/assets/js/all.js
@@ -1,5 +1,4 @@
-// var calcite   = require('../../../../dist/js/calcite-web')
-// var marketing = require('../../../../dist/js/calcite-web-marketing')
-
-calcite.extend(calciteMarketing)
-calcite.init()
+(function () {
+  calcite.extend(calciteMarketing);
+  calcite.init();
+})();
