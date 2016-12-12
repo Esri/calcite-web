512d620648908cce681c5ab140b6bce32cfffc1b
Paul C Pederson <paul.c.pederson@gmail.com>
2016-12-11 21:38:21 -0800

    fix syntax and formatting of bin scripts

--- bin/minify.js
+++ bin/minify.js
@@ -1,12 +1,12 @@
-var glob = require('glob')
-var minifier = require('minifier')
+var glob = require('glob');
+var minifier = require('minifier');
 
-glob('./dist/css/**.css', {ignore: './dist/css/**.min.css'}, minify)
-glob('./dist/js/**.js', {ignore: './dist/js/**.min.js'}, minify)
+glob('./dist/css/**.css', {ignore: './dist/css/**.min.css'}, minify);
+glob('./dist/js/**.js', {ignore: './dist/js/**.min.js'}, minify);
 
 function minify (err, files) {
   if (err) {
-    console.error('Oh no trubs!', err);
+    console.error('Error running minify script (bin/minify.js)', err);
     return;
   }
   files.forEach(function (file) {
--- bin/s3.js
+++ bin/s3.js
@@ -1,56 +1,55 @@
 var s3 = require('s3');
-var conf = require('./aws-credentials.json')
-var pkg = require('../package.json')
+var conf = require('./aws-credentials.json');
+var pkg = require('../package.json');
 
 var client = s3.createClient({
   s3Options: conf
 });
 
-var destination = "files/calcite-web/" + pkg.version + "/"
+var destination = 'files/calcite-web/' + pkg.version + '/';
 
 var versioned = {
-  localDir: "./dist",
+  localDir: './dist',
   s3Params: {
-    ACL: "public-read",
-    Bucket: "patterns.esri.com",
+    ACL: 'public-read',
+    Bucket: 'patterns.esri.com',
     Prefix: destination
   }
 };
 
 var latest = {
-  localDir: "./dist",
+  localDir: './dist',
   s3Params: {
-    ACL: "public-read",
-    Bucket: "patterns.esri.com",
-    Prefix: "files/calcite-web/latest/"
+    ACL: 'public-read',
+    Bucket: 'patterns.esri.com',
+    Prefix: 'files/calcite-web/latest/'
   }
 };
 
 var uploader = client.uploadDir(versioned);
 
-uploader.on('error', function(err) {
-  console.error("unable to sync:", err.stack);
+uploader.on('error', function (err) {
+  console.error('unable to sync:', err.stack);
 });
 
-uploader.on('progress', function() {
-  console.log("progress", uploader.progressAmount, uploader.progressTotal);
+uploader.on('progress', function () {
+  console.log('progress', uploader.progressAmount, uploader.progressTotal);
 });
 
-uploader.on('end', function() {
-  console.log("done uploading");
+uploader.on('end', function () {
+  console.log('done uploading');
 });
-
 
 var latestUpload = client.uploadDir(latest);
 
-uploader.on('error', function(err) {
-  console.error("unable to sync:", err.stack);
+uploader.on('error', function (err) {
+  console.error('unable to sync:', err.stack);
 });
 
-uploader.on('progress', function() {
-  console.log("progress", uploader.progressAmount, uploader.progressTotal);
+uploader.on('progress', function () {
+  console.log('progress', uploader.progressAmount, uploader.progressTotal);
 });
 
-uploader.on('end', function() {
-  console.log("done uploading");
+uploader.on('end', function () {
+  console.log('done uploading');
 });
