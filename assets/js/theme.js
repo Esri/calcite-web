1908607fb2fda88b8943b0a05e509aee52c85552
Adam Tirella <adam@tirel.la>
2018-03-22 13:33:26 -0700

    Updates

---
+++ assets/js/theme.js
@@ -0,0 +1,45 @@
+// Calcite docs theme switcher
+(function () {
+  var localStorageItem = 'calciteWebTheme';
+  var themeDarkStylesheet = document.getElementById('calcite-theme-dark');
+  var themeLightStylesheet = document.getElementById('calcite-theme-light');
+  var themeDarkAnimationClass = 'theme-dark-fade-in';
+  var themeLightAnimationClass = 'theme-light-fade-in';
+  var themeToggles = Array.prototype.slice.call(document.querySelectorAll('.docs-theme-toggle'));
+
+  document.addEventListener('DOMContentLoaded', setThemeOnLoad);
+  themeToggles.forEach(function (el) { return el.addEventListener('click', changeTheme); });
+
+  // set disabled attribute on load here as well as in markup for ff
+  function setThemeOnLoad() {
+    if (localStorage.getItem(localStorageItem) === 'dark') {
+      themeDarkStylesheet.disabled = false;
+      themeLightStylesheet.disabled = true;
+      themeToggles.forEach(function (el) { return el.checked = true });
+    } else {
+      themeLightStylesheet.disabled = false;
+      themeDarkStylesheet.disabled = true;
+      themeToggles.forEach(function (el) { return el.checked = false });
+    }
+  }
+
+  function changeTheme() {
+    if (localStorage.getItem(localStorageItem) === 'dark' || localStorage.getItem(localStorageItem) === null) {
+      localStorage.setItem(localStorageItem, 'light');
+      themeLightStylesheet.disabled = false;
+      themeDarkStylesheet.disabled = true;
+      document.body.classList.remove(themeDarkAnimationClass);
+      document.body.classList.add(themeLightAnimationClass);
+      themeToggles.forEach(function (el) { return el.checked = false });
+      setTimeout(function () { document.body.classList.remove(themeLightAnimationClass); }, 2000);
+    } else {
+      localStorage.setItem(localStorageItem, 'dark');
+      themeDarkStylesheet.disabled = false;
+      themeLightStylesheet.disabled = true;
+      document.body.classList.remove(themeLightAnimationClass);
+      document.body.classList.add(themeDarkAnimationClass);
+      themeToggles.forEach(function (el) { return el.checked = true });
+      setTimeout(function () { document.body.classList.remove(themeDarkAnimationClass); }, 2000);
+    }
+  }
+})();
