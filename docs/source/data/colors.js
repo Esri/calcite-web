debc2f2dea211571d2e4cf26443d984dc19037ae
Paul C Pederson <paul.c.pederson@gmail.com>
2018-09-18 17:31:33 -0700

    restore hex values in color swatches

--- docs/source/data/colors.js
+++ docs/source/data/colors.js
@@ -1,12 +1,13 @@
-var colors = require('@esri/calcite-colors');
+var scssToJson = require('masa-scss-to-json/masa-scss-to-json');
+var colors = scssToJson(process.cwd(), 'lib/sass/calcite-web/base/_colors.scss');
 
 Object.keys(colors).forEach(key => {
   // convert three digit hex codes to six digits
   if (colors[key].length < 5) {
     colors[key] = colors[key] + colors[key].substr(-3);
   }
-})
+});
 
 module.exports = function (callback) {
-  callback(null, colors);
+  return callback(null, colors);
 };
