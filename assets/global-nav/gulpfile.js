c12f5b975c8f8a1310a9ff48f1ec8b4572692308
Adam Tirella <adam@tirel.la>
2023-10-13 10:29:56 -0700

    Updates

--- assets/global-nav/gulpfile.js
+++
@@ -1,6 +0,0 @@
-﻿const gulp = require('gulp');
-
-require('require-dir')('./gulp-tasks');
-
-gulp.task('default', ["clean-dist", "watch-es-lint", "watch-js", "watch-postcss", "watch-optimize-images", "watch-static-files", "web-server"], () => {});
-gulp.task('build', ["clean-dist", "compile-js", "compile-postcss", "optimize-images", "copy-static-files"], () => {});
